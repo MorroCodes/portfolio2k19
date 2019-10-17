03b9230992188288a5f368b9a6880a2fe2de8f91
MorroCodes <46780762+MorroCodes@users.noreply.github.com>
2019-10-17 15:15:13 +0200

    remove page renderer

--- src/js/main.js
+++ src/js/main.js
@@ -4,14 +4,9 @@ import Highway from '@dogstudio/highway';
 
 import Fade from '../js/fade';
 
-import CustomRenderer from '../js/custom-renderer';
-
 // Call Highway.Core once.
 // Store it in a variable to use events
 const H = new Highway.Core({
-    renderers: {
-        name: CustomRenderer
-      },
     transitions: {
         default: Fade
       }
